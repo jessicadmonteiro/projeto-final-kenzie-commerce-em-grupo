1e916fdb5ce67a70e0c4570d30673da17f4b2f5e
Eduardo <eduardo.portela.jrr@gmail.com>
2023-05-03 10:18:28 -0300

    feat: criando model e serializer

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=127)
+    category = models.CharField(max_length=50)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    status = models.CharField(max_length=127)
+
+
+# user = models.ForeignKey(
+#     "users.User", on_delete=models.CASCADE, related_name="products"
+# )
+# cart = models.ManyToManyField(
+#     "cart.Cart",
+#     related_name="cart_products"
+#     )
