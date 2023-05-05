104398f91118f87d1b3106eb95b5f8b62a5de517
LucasDantas77 <lucasdantasrodrigues847@gmail.com>
2023-05-05 14:03:53 -0300

    feat:user finalizada, update, retrieve, delete adicionados

--- user/views.py
+++ user/views.py
@@ -1,6 +1,8 @@
+from user.permissions import UserAuth
 from .models import User
 from .serializers import UserSerializer
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class UserView(CreateAPIView):
@@ -10,3 +12,14 @@ class UserView(CreateAPIView):
     def perform_create(self, serializer):
         is_admin = self.request._data["is_admin"]
         serializer.save(is_admin=is_admin)
+
+
+class UserViewId(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [UserAuth]
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return User.objects.all()
